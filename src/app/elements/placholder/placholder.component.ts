7c7e505698448d475ab6fe3f78c63844c8be3db6
Gurcan Hamali <gurcanhamali@gmail.com>
2020-10-26 16:00:48 +1100

    108. Customizing Components

--- src/app/elements/placholder/placholder.component.ts
+++ src/app/elements/placholder/placholder.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-placholder',
   templateUrl: './placholder.component.html',
-  styleUrls: ['./placholder.component.css']
+  styleUrls: ['./placholder.component.css'],
 })
 export class PlacholderComponent implements OnInit {
+  @Input() header = true;
+  @Input() lines = 3;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
