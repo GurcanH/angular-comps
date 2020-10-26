608899f0a3ad441bfb728e59d75056592ecd2747
Gurcan Hamali <gurcanhamali@gmail.com>
2020-10-26 16:31:03 +1100

    113. Creating a Segment Component

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -6,9 +6,10 @@ import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlacholderComponent } from './placholder/placholder.component';
 import { TimesDirective } from './times.directive';
 import { SharedModule } from '../shared/shared.module';
+import { SegmentComponent } from './segment/segment.component';
 
 @NgModule({
-  declarations: [ElementsHomeComponent, PlacholderComponent, TimesDirective],
+  declarations: [ElementsHomeComponent, PlacholderComponent, TimesDirective, SegmentComponent],
   imports: [CommonModule, ElementsRoutingModule, SharedModule],
 })
 export class ElementsModule {}
