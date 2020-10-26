6b102f9c98911fd343650c2d8f246646378eb293
Gurcan Hamali <gurcanhamali@gmail.com>
2020-10-27 09:01:34 +1100

    139. Hiding the Modal with NgOnDestroy

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -11,4 +11,8 @@ export class ModalComponent implements OnInit {
   ngOnInit(): void {
     document.body.appendChild(this.el.nativeElement);
   }
+
+  ngOnDestroy() {
+    this.el.nativeElement.remove();
+  }
 }
