839f883a601cb19299f12e57507f9f61dbd52943
Gurcan Hamali <gurcanhamali@gmail.com>
2020-10-26 20:16:21 +1100

    129. Views Module Exercise

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
+import { ViewsHomeComponent } from './views-home/views-home.component';
+import { StaticsComponent } from './statics/statics.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ViewsHomeComponent, StaticsComponent],
   imports: [
     CommonModule,
     ViewsRoutingModule
