93f2b3ce4e57a54cd1ecaa233a36fa5b56065d6e
Gurcan Hamali <gurcanhamali@gmail.com>
2020-10-26 16:07:54 +1100

    109. Reminder on Structural Directives

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlacholderComponent } from './placholder/placholder.component';
+import { TimesDirective } from './times.directive';
 
 @NgModule({
-  declarations: [ElementsHomeComponent, PlacholderComponent],
+  declarations: [ElementsHomeComponent, PlacholderComponent, TimesDirective],
   imports: [CommonModule, ElementsRoutingModule],
 })
 export class ElementsModule {}
